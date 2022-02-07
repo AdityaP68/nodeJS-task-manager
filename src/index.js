00421afc79e1f34bc3b81136ff3e27c341d8d64d
Aditya Prakash <adityaprakash6688@gmail.com>
2022-02-07 13:33:36 +0530

    refactor for mongoose-endpoint interaction

--- src/index.js
+++ src/index.js
@@ -1,13 +1,26 @@
 const express = require('express')
+const User = require('./models/User')
+
+//check its functioning
+require('./db/mongoose')
 const app = express()
 const port = process.env.PORT || 3000
 
+//check its functioning
 app.use(express.json())
 
 app.post('/users',(req, res)=>{
-    console.log(req.body)
+    const user = new User(req.body)
 
-    res.send('testing')
+    
+    user.save().then((result)=>{
+                    res.send(result)
+                })
+                .catch((e)=>{
+                    res.status(400).send(e)
+                })
+    //cant send 2 response to the user only 1 response per request
+   // res.send('testing')
 })
 
 app.listen(port, ()=>{
